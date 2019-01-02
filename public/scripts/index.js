e40e0c76f0fa2a80e30046bc481f6f21e286199e
abtheteabandit <bradleybrooks@wustl.edu>
2019-01-01 23:26:54 -0600

    relocated embedded mega script in index.html to index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,180 @@
+// AB Brooks Zone
+
+var rain = null,
+drops = [],
+rainTimer = null,
+maxDrops = 15;
+
+function init(){
+
+	container = document.getElementById("container");
+	container.width = window.innerWidth;
+	container.height = window.innerHeight;
+
+	rain = document.getElementById("rain");
+	rain.width  = window.innerWidth;
+	rain.height  = window.innerHeight;
+
+	window.addEventListener('resize', function(){
+		console.log("resized");
+		rain.width  = window.innerWidth;
+		rain.height  = window.innerHeight;
+	});
+
+	rainTimer = setInterval(addDrop, 1000);
+	setInterval(animate, 30);
+	//alert(window.location.href);
+}
+
+function animate() {
+		Update();
+}
+
+function Update(){
+	for(var i = 0; i < drops.length; i++){
+		if(drops[i].paused){
+			// pause
+
+		}else{
+			if (drops[i].y < window.innerHeight){
+				drops[i].UpdatePosition(3);
+			}else{
+				//Remove the drop
+				var drop = drops[i].theDiv;
+				drop.parentNode.removeChild(drop);
+				drops.splice(i, 1);
+			}
+		}
+	}
+}
+
+
+function addDrop(){
+	if (drops.length == maxDrops){
+		// do nothing
+	}else{
+		drops[drops.length] = new Drop();
+		drops[drops.length-1].id = drops.length;
+	}
+}
+
+var pausedDrop = null;
+
+
+class Drop {
+
+	constructor(){
+		console.log("creating drop")
+		this.id = 0;
+		this.theDiv = document.createElement("div");
+		this.theDiv.className = "rainDrop";
+		this.width = 80;
+		this.height = 80;
+		this.x = Math.random()*(window.innerWidth);
+		this.y = -80;
+		this.UpdateDiv();
+
+		// var r = RandColor();
+		// var g = RandColor();
+		// var b = RandColor();
+		// this.theDiv.style.backgroundColor = "rgba("+r+","+g+","+b+",1.0)";
+		var colorString = RandColorRange();
+		this.theDiv.style.backgroundColor = colorString;
+		this.theDiv.paused = false;
+		this.theDiv.dropRef = this;
+		this.audio = new Audio('./assets/transvertion.mp3');
+		this.audio.type='audio/mp3';
+		this.theButton = document.createElement("p");
+		this.theButton.innerHTML = "text";
+		this.theDiv.appendChild(this.theButton);
+		rain.appendChild(this.theDiv);
+		this.AddClickToDiv();
+	}
+	playAudio() {
+		 var playPromise = this.audio.play();
+
+		 if (playPromise !== undefined) {
+				 playPromise.then(function () {
+						 console.log('Playing....');
+				 }).catch(function (error) {
+						 console.log('Failed to play....' + error);
+				});
+		}
+	}
+
+
+
+	TogglePaused(){
+		this.paused = !this.paused;
+		 if(this.paused){
+			 this.playAudio();
+			 }
+		else{
+			this.audio.pause();
+			}
+	}
+
+	AddClickToDiv(){
+		this.theDiv.addEventListener('click',function(){
+			this.dropRef.TogglePaused();
+		},false);
+	}
+
+	UpdateDiv(){
+		this.xString = this.x+"px";
+		this.yString = this.y+"px";
+		this.widthString = this.width+"px";
+		this.heightString = this.height+"px";
+		this.idString = "drop"+this.id;
+		this.theDiv.id = this.idString;
+		this.theDiv.style.width = this.widthString;
+		this.theDiv.style.height = this.widthString;
+		this.theDiv.style.left = this.xString;
+		this.theDiv.style.top = this.yString;
+	}
+
+	UpdatePosition(speed){
+		this.y += speed;
+		this.UpdateDiv();
+	}
+
+}
+
+function RandColor(){
+	return Math.random() * 255;
+}
+
+function RandColorRange(){
+	min = Math.ceil(1);
+	max = Math.floor(3);
+	num = Math.floor(Math.random() * (max - min + 1)) + min;
+	switch(num){
+		//rising blue
+		case 1:
+			newMin = 105;
+			newMax = 255;
+			blue = Math.floor(Math.random() * (newMax - newMin + 1)) + newMin;
+			return "rgba(255,0,"+blue+",1.0)";
+			break;
+		//diminishing red
+		case 2:
+			newMin = 0;
+			newMax = 255;
+			red = Math.floor(Math.random() * (newMax - newMin + 1)) + newMin;
+			return "rgba("+red+",0,255,1.0)";
+			break;
+		//rising green
+		case 3:
+			newMin = 0;
+			newMax = 156;
+			green = Math.floor(Math.random() * (newMax - newMin + 1)) + newMin;
+			return "rgba(0,"+green+",255,1.0)";
+			break;
+	}
+}
+
+// Sam Westerhack Zone
+
 // $("#search_button").addEventListener('click', searchHit);
 // $("#login_button").addEventListener('click',loginHit);
 // $("#sign_in_button").addEventListener('click',signInHit);
