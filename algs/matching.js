6b9157bc7f22ba954de4aa686df557d49b7ac4cb
Booth <alexanderrossbothe@gmail.com>
2019-01-08 02:47:33 -0600

    fixed algs, sam can u check if im handling the return of the async right, Im sure theres a better way to do it then just return the callback to the db

--- algs/matching.js
+++ algs/matching.js
@@ -67,152 +67,144 @@ var vibeMult=1;
 var timeMult=1;
 var timeEqualizer=1000;
 var priceEQ=10;
-var genresFromStr=[];
-var instsFromStr=[];
-var gigTypesFromStr=[];
-var vibesFromStr=[];
-var gigsToScore=[];
-var bandsToScore=[];
+
 
 //call this for a band searching for a gig, myBand is a json band and
-// gigs is an array of gig jsons and 
+// gigs is an array of gig jsons and
 //query String is the string they typed in search bar (i handled parsing)
-function findGigsForBand(myBand, gigs, queryStr){
-    parseQueryString(queryStr);
-    var queryStrScore=0;
-    for (theGig in gigs){
-        for (g in genresFromStr){
-          if theGig.genres.includes(g){
-            queryStrScroe+=(1*genreMult);
-          }
-        }
-        for (v in vibesFromStr){
-          if myGig.vibes.includes(v){
-            queryStrScroe+=(1*vibeMult);
-          }
-        }
-        for (t in gigTypesFromStr){
-          if myGig.types.includes(t){
-            queryStrScroe+=(1*typeMult);
-          }
-        }
-        for (i in instsFromStr){
-          if myGig.instruments.includes(i){
-            queryStrScroe+=(1*instMult);
+function findGigsForBand(myBand, queryStr, db){
+//  var sortedGigs=[];
+  return db.db('gigs').collection('gigs').find({}).toArray(function(err,result){
+    if (err){
+      console.log("There was error getting gigs from db:" + err);
+      return [];
+    }
+    else{
+      var gigs=result;
+      var genresFromStr=[];
+      var instsFromStr=[];
+      var gigTypesFromStr=[];
+      var vibesFromStr=[];
+      var gigsToScore=[];
+      var bandsToScore=[];
+      var categories={"genres":[genreBank,genresFromStr,genreMult], "insts":[instsBank,instsFromStr,instMult],"vibes":[vibesBank,vibesFromStr,vibeMult],"gigTypes":gigTypeBank,gigTypesFromStr,typeMult]};
+
+      var categories = parseQueryString(queryStr, categories);
+      var queryStrScore = 0;
+
+
+      for (theGig in gigs){
+        for (key in categories){
+          if categories.hasOwnProperty(key){
+            if (theGig.hasOwnProperty(key)){
+              var contents = categories[key];
+              var fromStr=contents[1];
+              var mult=contents[2];
+              for (word in fromStr){
+                if (theGig[key].includes(word)){
+                  queryStrScore+=(1*mult);
+                }
+              }
+            }
           }
         }
 
-        var priceDiff=Math.abs((theGig.price-myBand.price));
-        var priceScore= (-(priceDiff/priceEQ)*priceMult);
-        var diffX=Math.pow((theGig.lat-myBand.lat),2);
-        var diffY=Math.pow((theGig.lng-myBand.lng),2);
-        var distance = Math.pow((diffX+diffY),0.5);
-        var distScore = -(distance*distMult);
-        var dateDiffs=[];
+          var priceDiff=Math.abs((theGig.price-myBand.price));
+          var priceScore= (-(priceDiff/priceEQ)*priceMult);
+          var diffX=Math.pow((theGig.lat-myBand.lat),2);
+          var diffY=Math.pow((theGig.lng-myBand.lng),2);
+          var distance = Math.pow((diffX+diffY),0.5);
+          var distScore = -(distance*distMult);
+          var dateDiffs=[];
+          for (date in myBand.openDates){
+            var timeDiff=diff_minutes(myBand.openDates[0],myGig.startDate);
+            dateDiffs.push(timeDiff);
+          }
+          var minDiff = dateDiffs[0];
+          for (diff in dateDiffs){
+            if (diff < minDiff) {
+              minDiff = diff;
+            }
+          }
 
-        for (date in myBand.openDates){
+          var timeDiff = minDiff / timeEqualizer;
+          var timeScore= -timeDiff*timeMult;
+          var score = timeScore+distScore+priceScore+queryStrScore;
+
+          gigsToScore.push([theGig,score]);
+        }
+        var sortedGigs=sortDict(gigsToScore);
+        db.close();
+        return sortedGigs;
+    }
+  });
+}
+
+function findBandsForGig(myGig, categories, db){
+  return db.db('bands').collection('bands').find({}).toArray(function(err,result){
+    if (err){
+      console.log("There was error getting gigs from db:" + err);
+      return [];
+    }
+    else{
+      var bands = result;
+      var categories = parseQueryString(queryStr,categories);
+      for (theBand in bands){
+        for (key in categories){
+          if categories.hasOwnProperty(key){
+            if theGig.hasOwnProperty(key){
+              var contents = categories[key];
+              var fromStr=contents[1];
+              var mult=contents[2];
+              for (word in fromStr){
+                if (theBand[key].includes(word)){
+                  queryStrScore+=(1*mult);
+                }
+              }
+            }
+          }
+        }
+        var priceDiff = Math.abs( (myGig.price-  theBand.price) );
+        var priceScore = (-(priceDiff/priceEQ) * priceMult);
+        var dateDiffs = [];
+        for (date in theBand.openDates){
           var timeDiff=diff_minutes(myBand.openDates[0],myGig.startDate);
           dateDiffs.push(timeDiff);
         }
         var minDiff = dateDiffs[0];
         for (diff in dateDiffs){
-          if (diff < minDiff) {
-            minDiff = diff;
+          if (diff < minDiff){
+            minDiff=diff;
           }
         }
-
-        var timeDiff = minDiff / timeEqualizer;
-        var timeScore= -timeDiff*timeMult;
-        var score = timeScore+distScore+priceScore+queryStrScore;
-        gigsToScore.push([theGig,score]);
+        var timeDiff = (minDiff) / timeEqualizer;
+        var timeScore = -timeDiff * timeMult;
+        var ratingScore = theBand.rating*ratingMult;
+        var score = timeScore+priceScore+queryStrScore+ratingScore;
+        bandsToScore.push([theBand,score]);
+      }
+      var sortedBands=sortDict(bandsToScore);
+      return sortedBands;
     }
-    var sortedGigs=sortDict(gigsToScore);
-    return sortedGigs;
+  });
 }
 
-
-
-function findBandsForGig(myGig, bands, queryStr){
-  parseQueryString(queryStr);
-  for (theBand in bands){
-    for (g in genresFromStr){
-      if theBand.genres.includes(g){
-        queryStrScroe+=(1*genreMult);
-      }
-    }
-    for (v in vibesFromStr){
-      if theBand.vibes.includes(v){
-        queryStrScroe+=(1*vibeMult);
-      }
-    }
-    for (t in gigTypesFromStr){
-      if theBand.types.includes(t){
-        queryStrScroe+=(1*typeMult);
-      }
-    }
-    for (i in instsFromStr){
-      if theBand.instruments.includes(i){
-        queryStrScroe+=(1*instMult);
-      }
-    }
-
-    var priceDiff=Math.abs((myGig.price-theBand.price));
-    var priceScore= (-(priceDiff/priceEQ)*priceMult);
-    /*
-    var diffX=Math.pow((myGig.lat-theBand.lat),2);
-    var diffY=Math.pow((myGig.lng-theBand.lng),2);
-    var distance = Math.pow((diffX+diffY),0.5);
-    var distScore = -(distance*distMult);
-    */
-    var dateDiffs=[];
-
-    for (date in theBand.openDates){
-      var timeDiff=diff_minutes(myBand.openDates[0],myGig.startDate);
-      dateDiffs.push(timeDiff);
-    }
-    var minDiff = dateDiffs[0];
-    for (diff in dateDiffs){
-      if (diff<minDiff){
-        minDiff=diff;
-      }
-    }
-
-    var timeDiff = (minDiff)/timeEqualizer;
-    var timeScore = -timeDiff*timeMult;
-    var ratingScore = theBand.rating*ratingMult;
-    var score = timeScore+priceScore+queryStrScore+ratingScore;
-    bandsToScore.push([theBand,score]);
-  }
-  var sortedBands=sortDict(bandsToScore);
-  return sortedBands;
-}
-
-function parseQueryString(queryStr){
+function parseQueryString(queryStr, categories){
   var lowerCased = queryStr.toLowerCased();
-  var score=0;
-  for (g in genreBank){
-    if lowerCased.includes(g){
-      genresFromStr.push(g);
-    }
+  //iterates through the categories and pushes tmatchign
+  //words from the query string in the appropaite collection of words
+  for (var key in categories){
+      if (categories.hasOwnProperty(key)){
+          var contents = categories[key];
+          for (word in contents[0]){
+            if (queryStr.includes(word)){
+              contents[1].push(word);
+            }
+          }
+          categories[key]=contents;
+      }
   }
-  for (i in instBank){
-    if lowerCased.includes(i){
-      instsFromStr.push(i);
-    }
-  }
-  for (t in gigTypeBank){
-    if lowerCased.includes(t){
-      gigTypesFromStr.push(t);
-    }
-  }
-  for (v in vibeBank){
-    if lowerCased.includes(v){
-      vibesFromStr.push(v);
-    }
-  }
-
-  var words = lowerCased.split(" ");
-
+  return categories;
 }
 
 function diff_minutes(dt2, dt1) {
