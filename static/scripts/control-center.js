86038b5b2f5b76de32e9eafe718fbaad6af21ad5
abtheteabandit <bradleybrooks@wustl.edu>
2019-01-25 20:42:20 -0600

    created basic control center

---
+++ static/scripts/control-center.js
@@ -0,0 +1,16 @@
+function init(){
+
+}
+
+var contactsButton = document.getElementById('contacts-button');
+var contactsSidebar = document.getElementById('contacts-sidebar');
+var open = false;
+contactsButton.addEventListener("click",function(){
+  console.log("clicked, "+open);
+  if(open){
+    document.getElementById('contacts-sidebar').style.display = 'none';
+  }else{
+    document.getElementById('contacts-sidebar').style.display = 'block';
+  }
+  open = !open;
+});
