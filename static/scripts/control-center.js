2d2175b72fd499bb4903b66b51887459b4d316b6
abtheteabandit <bradleybrooks@wustl.edu>
2019-03-14 23:51:10 -0500

    scriptable booked events and (partially) open events

--- static/scripts/control-center.js
+++ static/scripts/control-center.js
@@ -16,6 +16,215 @@
 
 //must implement getting the session data;
 //var username = getusernamefromsession;
+
+
+class BookedGig {
+
+  constructor(gig){
+    this.container = document.createElement("div");
+    this.container.className = "booked-gig";
+    this.titleEl = document.createElement("h3");
+    this.titleEl.innerHTML = gig.name;
+    this.locEl = document.createElement("h3");
+    this.locEl.innerHTML = gig.address;
+    this.gigContent = document.createElement("div");
+    this.gigContent.className = "gig-content";
+    this.gigImg = document.createElement("div");
+    this.gigImg.className = "gig-image";
+    this.gigPic = document.createElement("img");
+    this.gigPic.className = "gig-pic";
+    this.gigPic.src = gig.picture;
+    this.gigPicFrame = document.createElement("img");
+    this.gigPicFrame.className = "gig-pic-frame";
+    this.gigPicFrame.src = "../static/assets/Home/orangebox.png";
+    this.gigDesc = document.createElement("div");
+    this.gigDesc.className = "gig-description";
+    this.gigDescP = document.createElement("p");
+    this.gigDescP.className = "gig-description-p";
+    this.gigDescP.innerHTML = gig.description;
+    this.gigAct = document.createElement("div");
+    this.gigAct.className = "gig-act";
+    this.actPic = document.createElement("img");
+    this.actPic.className = "act-pic";
+    this.actPic.src = gig.bandFor.picture;
+    this.actPicFrame = document.createElement("img");
+    this.actPicFrame.className = "act-pic-frame";
+    this.actPicFrame.src = "../static/assets/Home/orangebox.png";
+    this.actNameplate = document.createElement("div");
+    this.actNameplate.className = "act-nameplate";
+    this.actName = document.createElement("p");
+    this.actName.className = "act-name";
+    this.actName.innerHTML = gig.bandFor.name;
+    this.gigConfirm = document.createElement("div");
+    this.gigConfirm.className = "gig-confirm";
+    this.gigConfirmP = document.createElement("p");
+    this.gigConfirmP.className = "gig-confirm-p";
+    this.gigConfirmP.innerHTML = "upon completion of this event, enter the confirmation code:";
+    this.gigConfirmInput = document.createElement("input");
+    this.gigConfirmInput.className = "gig-confirm-input";
+    this.gigConfirmA = document.createElement("a");
+    this.gigConfirmA.className = "gig-confirm-a";
+    this.gigConfirmA.href = "#"; // can be changed to a javascript function for code submission
+    this.gigConfirmA.innerHTML = "confirm";
+
+// tier 4
+    this.actNameplate.append(this.actName);
+// tier 3
+    this.gigImg.append(this.gigPic);
+    this.gigImg.append(this.gigPicFrame);
+
+    this.gigDesc.append(this.gigDescP);
+
+    this.gigAct.append(this.actPic);
+    this.gigAct.append(this.actPicFrame);
+    this.gigAct.append(this.actNameplate);
+
+    this.gigConfirm.append(this.gigConfirmP);
+    this.gigConfirm.append(this.gigConfirmInput);
+    this.gigConfirm.append(this.gigConfirmA);
+// tier 2
+    this.gigContent.append(this.gigImg);
+    this.gigContent.append(this.gigDesc);
+    this.gigContent.append(this.gigAct);
+    this.gigContent.append(this.gigConfirm);
+// tier 1
+    this.container.append(this.titleEl);
+    this.container.append(this.locEl);
+    this.container.append(this.gigContent);
+// tier 0
+    bookedGigs.append(this.container);
+  }
+}
+
+function testBookedGigs(){
+  var aGig = {
+    name: "the rum house",
+    address: "4487 big street, St. Louis, MO 63112",
+    picture: "../static/assets/Home/Art/6.jpeg",
+    description: "we are a very cool venue, looking for jazzy vibes and cool cats :)",
+    bandFor: {
+      name: "king gizzard and the lizard wizard",
+      picture: "../static/assets/Home/Art/9.jpeg"
+    }
+  };
+  var testingBooked = new BookedGig(aGig);
+}
+
+class OpenGig{
+  constructor(gig){
+    this.container = document.createElement("div");
+    this.container.className = "open-gig";
+    this.info = document.createElement("div");
+    this.info.className = "open-gig-info";
+    this.gigImg = document.createElement("div");
+    this.gigImg.className = "open-gig-image";
+    this.title = document.createElement("h3");
+    this.title.innerHTML = gig.name;
+    this.gigPic = document.createElement("img");
+    this.gigPic.className = "gig-pic";
+    this.gigPic.src = gig.picture;
+    this.gigPicFrame = document.createElement("img");
+    this.gigPicFrame.className = "gig-pic-frame";
+    this.gigPicFrame.src = "../static/assets/Home/orangebox.png";
+    this.gigDesc = document.createElement("div");
+    this.gigDesc.className = "open-gig-description";
+    this.gigDescH = document.createElement("h3");
+    this.gigDescH.innerHTML = "Description";
+    this.gigDescT = document.createElement("textarea");
+    this.gigDescT.className = "open-gig-textarea";
+    this.gigDescT.innerHTML = gig.description;
+    this.gigDT = document.createElement("div");
+    this.gigDT.className = "open-gig-date-time";
+    this.gigDTH = document.createElement("h3");
+    this.gigDTH.innerHTML = "Date";
+    this.gigDate = document.createElement("input");
+    this.gigDate.className = "open-gig-date";
+    this.gigDate.type = "date";
+    // this.gigDate.value = gig.date;
+    this.gigTimeL = document.createElement("h3");
+    this.gigTimeL.id = "open-gig-time-label";
+    this.gigTimeL.innerHTML = "Time";
+    this.gigSTL = document.createElement("label");
+    this.gigSTL.for = "open-gig-start-time";
+    this.gigSTL.innerHTML = "from"
+    this.gigST = document.createElement("input");
+    this.gigST.className = "open-gig-start-time";
+    this.gigST.type = "time";
+    // this.gigST.value = gig.startTime;
+    this.gigETL = document.createElement("label");
+    this.gigETL.for = "open-gig-end-time";
+    this.gigETL.innerHTML = "to";
+    this.gigET = document.createElement("input");
+    this.gigET.className = "open-gig-end-time";
+    this.gigET.type = "time";
+    // this.gigET.value = gig.endTime;
+    this.gigLP = document.createElement("div");
+    this.gigLP.className = "open-gig-loc-pay";
+    this.gigLPH = document.createElement("h3");
+    this.gigLPH.innerHTML = "Location";
+    this.gigLoc = document.createElement("input");
+    this.gigLoc.className = "open-gig-loc";
+    this.gigLoc.value = gig.address;
+    this.gigPL = document.createElement("h3");
+    this.gigPL.id = "open-gig-pay-label";
+    this.gigPL.innerHTML = "Max Pay ($/hr)";
+    this.gigPay = document.createElement("input");
+    this.gigPay.className = "max-pay-input";
+    // this.gigPay.value = gig.payment;
+    this.gigConfirm = document.createElement("a");
+    this.gigConfirm.href = "#";
+    this.gigConfirm.className = "open-gig-confirm";
+    this.gigConfirm.innerHTML = "confirm changes";
+
+    // tier 3
+    this.gigImg.append(this.title);
+    this.gigImg.append(this.gigPic);
+    this.gigImg.append(this.gigPicFrame);
+
+    this.gigDesc.append(this.gigDescH);
+    this.gigDesc.append(this.gigDescT);
+
+    this.gigDT.append(this.gigDTH);
+    this.gigDT.append(this.gigDate);
+    this.gigDT.append(this.gigTimeL);
+    this.gigDT.append(this.gigSTL);
+    this.gigDT.append(this.gigST);
+    this.gigDT.append(this.gigETL);
+    this.gigDT.append(this.gigET);
+
+    this.gigLP.append(this.gigLPH);
+    this.gigLP.append(this.gigLoc);
+    this.gigLP.append(this.gigPL);
+    this.gigLP.append(this.gigPay);
+    this.gigLP.append(this.gigConfirm);
+    // tier 2
+    this.info.append(this.gigImg);
+    this.info.append(this.gigDesc);
+    this.info.append(this.gigDT);
+    this.info.append(this.gigLP);
+    // tier 1
+    this.container.append(this.info);
+    // tier 0
+    openGigs.append(this.container);
+  }
+}
+
+function testOpenGigs(){
+  var aGig = {
+    name: "the rum house",
+    address: "4487 big street, St. Louis, MO 63112",
+    picture: "../static/assets/Home/Art/6.jpeg",
+    description: "we are a very cool venue, looking for jazzy vibes and cool cats :)",
+    bandFor: {
+      name: "king gizzard and the lizard wizard",
+      picture: "../static/assets/Home/Art/9.jpeg"
+    }
+  };
+  var testingOpen = new OpenGig(aGig);
+}
+
+
+
 function updateBand(id, query){
   $.post('/updateBand', {'_id':id, 'query':query}, result =>{
     alert(JSON.stringify(result));
@@ -58,47 +267,74 @@ function createWebPage(user){
   //loadGigs(user);
 }
 
-
+var profileGigs = null;
+var bookedGigs = null;
+var openGigs = null;
 
 function init(){
+  profileGigs = document.getElementById("profile-gigs");
 
-  var upcoming1 = {
-    id: "up1"
-  };
-  var upcoming2 = {
-    id: "up2"
-  };
+  //// IF a profile has booked gigs,
+  // bookedGigs = document.createElement("div");
+  // bookedGigs.className = "booked-gigs";
+  // var bookedGigsH = document.createElement("h2");
+  // bookedGigsH.innerHTML = "Booked Events";
+  // bookedGigs.append(bookedGigsH);
+  // profileGigs.append(bookedGigs);
+  //// then, fill the booked gigs section
+  // testBookedGigs();
 
-  var application1 = {
-    id: "app1"
-  };
+  // IF a profile has open gigs,
+  openGigs = document.createElement("div");
+  openGigs.className = "open-gigs";
+  var openGigsH = document.createElement("h2");
+  openGigsH.innerHTML = "Open Events";
+  openGigs.append(openGigsH);
+  profileGigs.append(openGigs);
+  // then, fill the open gigs section
+  testOpenGigs();
 
-  var application2 = {
-    id: "app2"
-  };
 
-  var past1 = {
-    id: "p1"
-  };
 
-  var past2 = {
-    id: "p2"
-  };
 
-  var band0 = {
-    name: "electric orchestra",
-    id: "band-0",
-    upcoming: [upcoming1, upcoming2, upcoming1, upcoming2, upcoming1],
-    applications: [application1, application2],
-    past: [past1, past2]
-  };
 
-  var user = {
-    band: [band0]
-  };
-  console.log(band0.name);
+  // var upcoming1 = {
+  //   id: "up1"
+  // };
+  // var upcoming2 = {
+  //   id: "up2"
+  // };
+  //
+  // var application1 = {
+  //   id: "app1"
+  // };
+  //
+  // var application2 = {
+  //   id: "app2"
+  // };
+  //
+  // var past1 = {
+  //   id: "p1"
+  // };
+  //
+  // var past2 = {
+  //   id: "p2"
+  // };
+  //
+  // var band0 = {
+  //   name: "electric orchestra",
+  //   id: "band-0",
+  //   upcoming: [upcoming1, upcoming2, upcoming1, upcoming2, upcoming1],
+  //   applications: [application1, application2],
+  //   past: [past1, past2]
+  // };
+  //
+  // var user = {
+  //   band: [band0]
+  // };
+  // console.log(band0.name);
   //loadBands(user);
-  getUsername();
+  // getUsername();
 }
 
 function loadBands(user){
