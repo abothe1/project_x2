420adbe25304cb701eff17eda957c02bec867f80
Alex Bothe <Bothe@Alexs-MacBook-Pro.local>
2019-05-13 00:15:31 -0500

    fixed another search bug

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -391,9 +391,12 @@ function searchForBands(){
   var m = document.getElementById("m");
   var mv = document.getElementById("mv");
   var v = document.getElementById("v");
-  m.src = "/assets/Search/m_filter_selected.png";
-  mv.src = "/assets/Search/mv_filter.png";
-  v.src = "/assets/Search/v_filter.png";
+  //m.src = "/assets/Search/m_filter_selected.png";
+  //mv.src = "/assets/Search/mv_filter.png";
+  //v.src = "/assets/Search/v_filter.png";
+  var noResP = document.getElementById("no-res-p");
+    noResP.innerHTML = "Sorry, we could not find any results. Try searching again with different keywords, or searching as one of your bands or events. For example, 'band with guitar for a birthday party' as an event.";
+    noResP.style.display = "none";
 
   var searchAsName = $('#selectDrop option:selected').data();
   console.log(JSON.stringify(searchAsName));
@@ -459,10 +462,13 @@ function searchForGigs(){
   var m = document.getElementById("m");
   var mv = document.getElementById("mv");
   var v = document.getElementById("v");
-  m.src = "/assets/Search/m_filter.png";
+  /*m.src = "/assets/Search/m_filter.png";
   mv.src = "/assets/Search/mv_filter.png";
   v.src = "/assets/Search/v_filter_selected.png";
-
+*/
+var noResP = document.getElementById("no-res-p");
+  noResP.innerHTML = "Sorry, we could not find any results. Try searching again with different keywords, or searching as one of your bands or events. For example, 'band with guitar for a birthday party' as an event.";
+  noResP.style.display = "none";
   var searchAsName = $('#selectDrop option:selected').data()['objid'];
   var serachAsType = $('#selectDrop option:selected').val();
   var searchText = $('#search_input').val();
